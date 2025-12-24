2bf2340a8bf241a74bb37458b592493bcad9b0ca
Shruti Vedha <shrutivedha1@gmail.com>
2025-12-24 20:13:27 +0530

    Add division problem

---
+++ Hackerrank_Solutions/Python_basics/division.py
@@ -0,0 +1,20 @@
+Q3) Task
+The provided code stub reads two integers, a and b, from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division, a // b. The second line should contain the result of float division, a/b .
+
+No rounding or formatting is necessary.
+
+SOLUTION:
+
+def div(a,b):
+    print(a//b)
+    print(a/b)
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    div(a,b)
+    
+
