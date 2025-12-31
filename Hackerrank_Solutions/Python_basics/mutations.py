45574f73920fe8a77a800dd6333bcba2ae7452e2
Shruti Vedha <shrutivedha1@gmail.com>
2026-01-01 00:07:46 +0530

    Add mutations.py

---
+++ Hackerrank_Solutions/Python_basics/mutations.py
@@ -0,0 +1,27 @@
+Q8) Task:
+Read a given string, change the character at a given index and then print the modified string.
+Function Description
+
+Complete the mutate_string function in the editor below.
+
+mutate_string has the following parameters:
+
+string string: the string to change
+int position: the index to insert the character at
+string character: the character to insert
+Returns
+
+string: the altered string
+
+
+SOLUTIONS:
+
+def mutate_string(string, position, character):
+    string=string[:position] +character+ string[position+1:]
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
